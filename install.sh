1dc6252794ebd6ef9f7c149b565225743e7134e3
mYahagi <myahagi@hey.jp>
2022-05-22 01:54:38 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 DOT_DIR=~/app/dotfiles
 
-git clone https://github.com/mYahagi/dotfiles.git ${DOT_DIR}
-
 ln -sf $DOT_DIR/.zshrc ~/.zshrc
 ln -sf $DOT_DIR/vim/dein.toml ~/.config/nvim/dein.toml
 ln -sf $DOT_DIR/vim/index.vim ~/.config/nvim/index.vim
