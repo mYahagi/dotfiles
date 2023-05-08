2018c28fbf68a98fc5ada364522e50c0edf07c4c
Masakazu Yahagi <y.masa7779@gmail.com>
2023-05-08 18:38:47 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 DOT_DIR=~/app/dotfiles
 
 mkdir -p ~/.config/nvim/
-touch ~/.config/nvim/dein.toml ~/.config/nvim/index.vim
+touch ~/.config/nvim/dein.toml ~/.config/nvim/init.vim
 
 ln -sf $DOT_DIR/vim/dein.toml ~/.config/nvim/dein.toml
 ln -sf $DOT_DIR/vim/init.vim ~/.config/nvim/init.vim
