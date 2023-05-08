0fc660c9eb6dd366d91f0a46b74fd2542f6cb8fc
Masakazu Yahagi <y.masa7779@gmail.com>
2023-05-08 18:38:24 +0900

    fix link

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.config/nvim/
 touch ~/.config/nvim/dein.toml ~/.config/nvim/index.vim
 
 ln -sf $DOT_DIR/vim/dein.toml ~/.config/nvim/dein.toml
-ln -sf $DOT_DIR/vim/index.vim ~/.config/nvim/index.vim
+ln -sf $DOT_DIR/vim/init.vim ~/.config/nvim/init.vim
