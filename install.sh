2418cb681b7f6d4876677cd563aac62f5b1ece94
Masakazu Yahagi <y.masa7779@gmail.com>
2023-05-09 18:57:39 +0900

    fix

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ touch ~/.config/nvim/dein.toml ~/.config/nvim/init.vim
 
 ln -sf $DOT_DIR/vim/dein.toml ~/.config/nvim/dein.toml
 ln -sf $DOT_DIR/vim/init.vim ~/.config/nvim/init.vim
+
+# For nodebrew
+mkdir -p ~/.nodebrew/src
